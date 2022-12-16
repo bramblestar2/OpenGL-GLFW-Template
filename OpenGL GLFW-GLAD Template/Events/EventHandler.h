350f14b7186bb274163113f1111ebf2c2a63c7d6
Jay Feather2000 <thegals4321@gmail.com>
2022-12-16 09:58:51 -0600

    Fixed drop event

--- OpenGL GLFW-GLAD Template/Events/EventHandler.h
+++ OpenGL GLFW-GLAD Template/Events/EventHandler.h
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 #include <GL/glew.h>
 #include <vector>
+#include <string>
 
 enum class EventTypes
 {
@@ -74,7 +75,7 @@ protected:
 	struct Drop
 	{
 		int count;
-		const char** paths;
+		std::string* paths;
 	};
 
 	//Window events
@@ -159,10 +160,21 @@ protected:
 /// Simple event handling
 /// 
 public:
+	~EventHandler()
+	{
+		if (topEventType == EventTypes::Drop)
+			if (topEvent.drop.paths != nullptr)
+				delete[] topEvent.drop.paths;
+	}
+	
 	void pop_event() 
 	{ 
 		if (!isEmpty())
 		{
+			if (topEventType == EventTypes::Drop)
+				if (topEvent.drop.paths != nullptr)
+					delete[] topEvent.drop.paths;
+			
 			topEvent = eventList.at(eventList.size() - 1);
 			topEventType = eventTypeList.at(eventTypeList.size() - 1);
 			eventTypeList.pop_back();
